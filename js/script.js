e1c80b2207de4b5981b294c1fafaf0e73b031c4e
roman <vokadusomar@gmail.com>
2021-10-29 17:12:31 +0300

    task33-2

--- js/script.js
+++ js/script.js
@@ -58,6 +58,9 @@ document.addEventListener('DOMContentLoaded', () => {
             if (newFilm.length > 21) {
                 newFilm = `${newFilm.substring(0, 22)}...`;
             }
+            if (favorite) {
+                alert('Добавляем любимый фильм')
+            }
 
             movieDB.movies.push(newFilm);
             sortArr(movieDB.movies);
@@ -99,7 +102,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function createMovieList(films, parent) {
         parent.innerHTML = "";
-    
+        sortArr(films);
         
         films.forEach((film, i) => {
             parent.innerHTML += `
@@ -109,10 +112,20 @@ document.addEventListener('DOMContentLoaded', () => {
                 
             `;
         });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                // createMovieList(movieDB.movies, movieList);
+                createMovieList(films, parent);
+
+            });
+        });
     }
     deleteAdv(adv);
     makeChanges();
-    sortArr(movieDB.movies);
+
     
     createMovieList(movieDB.movies, movieList);
     
